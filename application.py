b27a56cc5ff64db136f4f67d29c0c4d88dedfc15
Madhusudhan Konda <m.konda@chocolateminds.com>
2024-11-05 14:46:58 +0000

    Updated code

--- application.py
+++ application.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 # Default route
 @app.route("/")
 def home():
-    return "<h1>Hello, Azure Gurus!! You are all Amazing!</h1>"
+    return "<h1>Hello, Azure!! You are all Amazing Professionals!!</h1>"
